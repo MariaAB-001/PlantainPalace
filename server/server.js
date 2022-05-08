07f8b97c124a92e780a9522a42ec003e90d65f26
MariaAB-001 <mpab96@gmail.com>
2022-05-07 22:05:13 -0400

    order model (backend)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const cors = require("cors"); //cors lets backend & frontend talk 
 const app = express();
 const port = 8000;
+
+app.use(express.json()); // allow reeading json objects
+app.use(express.urlencoded({extended: true}));
+app.use(cors({
+    origin: "http://localhost:3000" //lets 3000 talk to 8000 
+}))
+
+require("./config/mongoose.config"); //importing db logic from mongoose config file
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
